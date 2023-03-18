52b9a42a43d0920b0255f8d16a851182b1324545
your name <your-email@foo.com>
2023-03-18 11:40:21 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution here!
+
+
+       const cats=["Milo","Otis","Garfield"]
+
+       function destructivelyAppendCat(name) {
+           cats.push(name)
+       }
+
+       function destructivelyPrependCat(name) {
+           cats.unshift(name)
+       }
+
+       function destructivelyRemoveLastCat() {
+           cats.pop()
+       }
+
+       function destructivelyRemoveFirstCat() {
+           cats.shift()
+       }
+
+       function appendCat(name) {
+           const newcat=[...cats,name]
+           return newcat
+       }
+
+       function prependCat(name) {
+           const cats=[name,...cats]
+           return cats
+       }
+
+       function removeLastCat() {
+           const cats3=cats.slice(0,-1)
+           return cats3
+       }
+
+       function removeFirstCat() {
+           const cats4=cats.slice(1)
+           return cats4
+       }
